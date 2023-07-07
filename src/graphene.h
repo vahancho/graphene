340aa904b25426164aead035c79d4ffc18d351e9
Vahan Aghajanyan <vahancho@gmail.com>
2023-07-07 10:12:11 +0200

    Add function to calculate the shortest path between two nodes.
    The function uses Dijkstra algorithm for the shortest path. The
    weight function is abstract and can be defined by client, i.e.
    it must be a function that returns weight on an edge.

--- src/graphene.h
+++ src/graphene.h
@@ -27,6 +27,7 @@
 
 #include <map>
 #include <set>
+#include <queue>
 #include <vector>
 
 enum class GraphType
@@ -59,6 +60,23 @@ public:
 
     /// Two nodes \p x and \p y are adjacent if {x, y} is an edge
     bool adjacent(const NodeType &x, const NodeType &y) const;
+
+    /// Returns the shortest path from the node \p from to the node \p to.
+    /*!
+        The function uses the Dijkstra algorithms for the shortest path between
+        two nodes. The \p weightFunction is a custom function that returns weight
+        that corresponds to two nodes (edge). For example, it can be a distance
+        between two geometrical points.
+
+        \param from The source node
+        \param to The target node
+        \param weightFunction A function that calculates a weight for an edge (between to nodes)
+        \return A shortest path (list of nodes) between two nodes.
+    */
+    template <typename Func>
+    std::vector<NodeType> shortestPath(const NodeType &from,
+                                       const NodeType &to,
+                                       Func weightFunction) const;
 
 private:
     std::map<NodeType, std::set<NodeType>> m_adjacencyList;
@@ -127,6 +145,103 @@ bool Graphene<NodeType, GT>::adjacent(const NodeType &x, const NodeType &y) cons
         }
     }
     return false;
+}
+
+template<typename NodeType, GraphType GT>
+template <typename Func>
+std::vector<NodeType> Graphene<NodeType, GT>::shortestPath(const NodeType &from,
+                                                           const NodeType &to,
+                                                           Func weight) const
+{
+    if (m_adjacencyList.find(from) == m_adjacencyList.cend() ||
+        m_adjacencyList.find(to) == m_adjacencyList.cend()) {
+        return {};
+    }
+
+    using WeightType = decltype(weight(from, to));
+    using Pair = std::pair<WeightType, NodeType>;
+
+    // A priority queue - the smallest element on top
+    std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair>> queue;
+
+    // The node's weight abstraction.
+    class Weight
+    {
+    public:
+        using Path = std::vector<NodeType>;
+
+        Weight() = default;
+        Weight(const NodeType &from) : m_path{from} {}
+        bool infinite() const { return m_infinite; }
+        void setWeight(WeightType weight) { m_weight= weight; m_infinite = false; }
+        WeightType weight() const { return m_weight; }
+        const Path &path() const { return m_path; }
+        Path &path() { return m_path; }
+
+    private:
+        WeightType m_weight{};
+        bool m_infinite{true};
+        Path m_path;
+    };
+
+    std::map<NodeType, Weight> nodeWeights;
+
+    // Initialize with the source node.
+    queue.push({WeightType{}, from});
+    nodeWeights.emplace(from, Weight{from});
+
+    while (!queue.empty()) {
+        // Make a copy!
+        auto node = queue.top().second;
+        queue.pop();
+        const auto &nodeWeight = nodeWeights[node];
+
+        auto it = m_adjacencyList.find(node);
+        if (it != m_adjacencyList.cend()) {
+            for (const auto &adjacent : it->second) {
+                auto edgeWeight = weight(node, adjacent);
+
+                // If there is shorted path to 'adjacent' through 'node'.
+                auto &adjacentWeight = nodeWeights[adjacent];
+                const auto totalWeight = nodeWeight.weight() + edgeWeight;
+
+                if (adjacentWeight.infinite() || (adjacentWeight.weight() > totalWeight)) {
+                    adjacentWeight.setWeight(totalWeight);
+
+                    auto &adjacentPath = adjacentWeight.path();
+                    auto &nodePath = nodeWeight.path();
+
+                    adjacentPath.insert(adjacentPath.end(),
+                                        nodePath.begin(),
+                                        nodePath.end());
+                    adjacentPath.emplace_back(adjacent);
+
+                    queue.push({adjacentWeight.weight(), adjacent});
+                }
+            }
+        }
+    }
+
+    return nodeWeights[to].path();
+
+    /*
+    printf("Node\t\tDistance\tPath\n");
+    for (const auto &i : nodeWeights) {
+        if (i.first == to) {
+            const auto &path = i.second.path();
+            ret = path;
+
+            std::cout << i.first << "\t\t" << i.second.weight() << "\t\t";
+            for (const auto &p : path) {
+                std::cout << p << "-->";
+            }
+            std::cout << '\n';
+            break;
+        }
+    }
+
+    return ret;
+    */
 }
 
 #endif // !__GRAPHENE_H__
